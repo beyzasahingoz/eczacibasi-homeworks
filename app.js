6292586cf6cf7d8eb8f624b865931b32ea4beef7
beyza <70069234+beyzasahingoz@users.noreply.github.com>
2023-05-07 23:34:48 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ import axios from 'axios'; //axios kütüphanesi import edildi.
 
 async function getData(userId) {
     //user için istek yollandı.
-    const { data: user } = await axios
+    const { data: firstReq } = await axios
         ("https://jsonplaceholder.typicode.com/users/${id}");
     //post için istek yollandı.
-    const { data: posts } = await axios
+    const { data: secondReq } = await axios
         ("https://jsonplaceholder.typicode.com/posts?userId=${id}");
     //istekler yeni istek altında toplandı ve konsolda görüntülendi.
     const newReq = { firstReq, Posts: secondReq }; 
